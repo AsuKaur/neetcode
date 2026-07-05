bbe292571cc1b9ac9310cd8f0d35c7b9eafbf367
Asu Kaur <43930830+AsuKaur@users.noreply.github.com>
2026-07-05 23:00:14 +0100

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minBuy=prices[0];
+        int maxP=0;
+        for(int p:prices){
+            maxP=Math.max(maxP,p-minBuy);
+            minBuy=Math.min(minBuy,p);
+        }
+        return maxP;
+    }
+}
