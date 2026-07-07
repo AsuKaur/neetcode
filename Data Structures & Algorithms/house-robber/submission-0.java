b91980648a9b57eb85c4e1f2684ba0ba6a9322a9
Asu Kaur <43930830+AsuKaur@users.noreply.github.com>
2026-07-07 20:47:39 +0100

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int rob(int[] nums) {
+        int rob1=0,rob2=0;
+        for(int n:nums){
+            int tmp=Math.max(n+rob1,rob2);
+            rob1=rob2;
+            rob2=tmp;
+        }
+        return rob2;
+    }
+}
