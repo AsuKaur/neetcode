9a23189356597173a9bdafe7a1cf4d6dbdcfbd5a
Asu Kaur <43930830+AsuKaur@users.noreply.github.com>
2026-07-07 23:27:46 +0100

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int reverseBits(int n) {
+        int res=0;
+        for(int i=0;i<32;i++){
+            int bits=(n>>i)&1;
+            res=res+(bits<<(31-i));
+        }
+        return res;
+    }
+}
